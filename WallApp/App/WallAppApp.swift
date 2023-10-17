ff45dacc4c7296c98b3c9b4e19f139d26191b8d5
0xJs <jpsaad00@gmail.com>
2023-10-16 21:22:29 -0400

    [Section 0.0]: Project Initialization - Set up Wall App base structure
    Details:
    
    Created iOS project in Xcode with Swift.
    Set up foundational directory structure for the project.
    
    Reason for Change:
    
    Starting the development of the Wall App.
    Affected Modules/Files:
    
    WallAppApp.swift
    LoginView
    
    Related Issues/Tasks:
    
    #1 - Initial project setup.
    
    ---

---
+++ WallApp/App/WallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallAppApp.swift
+//  WallApp
+//
+//  Created by 0xJs on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
